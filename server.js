8ebc1fce7606cf2b3ba255864dadec24586bb309
Danilo <danilo.m.gustavo@gmail.com>
2024-09-30 13:33:12 -0300

    Atualização de localizador

--- server.js
+++ server.js
@@ -27,11 +27,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Rota principal para receber dados do OwnTracks (ou Traccar)
 app.post('/', async (req, res) => {
   try {
-    const { lat, lon, vel, tst, tid, batt } = req.body;
+    const { _type, lat, lon, vel, tst, tid, batt } = req.body;
+
+    // Verificar se o tipo de mensagem é "location" (próprio do OwnTracks)
+    if (_type !== 'location') {
+      return res.status(400).json({ message: 'Tipo de mensagem inválido!' });
+    }
 
     // Verifica se os dados obrigatórios estão presentes
-    if (!lat || !lon) {
-      return res.status(400).json({ message: 'Dados inválidos!' });
+    if (!lat || !lon || !tst || !tid) {
+      return res.status(400).json({ message: 'Dados de localização inválidos!' });
     }
 
     const timestamp = new Date(tst * 1000); // Convertendo timestamp UNIX para formato de data
@@ -41,11 +46,9 @@ app.post('/', async (req, res) => {
         `;
     await pool.query(query, [tid, lat, lon, vel || 0, timestamp, batt || 100]);
 
-    // Garantir que uma resposta seja enviada corretamente
     return res.status(200).json({ message: 'Dados recebidos com sucesso!' });
   } catch (error) {
     console.error('Erro ao processar dados: ', error);
-    // Enviar resposta de erro caso algo dê errado
     return res.status(500).json({ message: 'Erro no servidor ao processar dados.' });
   }
 });
