a78dcb1e4a778abdb0c839a1ad1ad07d7f9f3034
Danilo <danilo.m.gustavo@gmail.com>
2024-09-30 13:25:33 -0300

    Atualização de localizador

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const bodyParser = require('body-parser');
 const { Pool } = require('pg');
 const path = require('path');
 
-
 const app = express();
 app.use(bodyParser.json());
+
+// Definir o tempo limite para requisições
+app.use((req, res, next) => {
+  req.setTimeout(300000); // 5 minutos
+  next();
+});
 
 // Usar a variável de ambiente para o banco de dados
 const pool = new Pool({
@@ -19,7 +24,7 @@ const pool = new Pool({
 // Servir arquivos estáticos do diretório "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rota principal para receber dados do OwnTracks
+// Rota principal para receber dados do OwnTracks (ou Traccar)
 app.post('/', async (req, res) => {
   try {
     const { lat, lon, vel, tst, tid, batt } = req.body;
@@ -31,15 +36,17 @@ app.post('/', async (req, res) => {
 
     const timestamp = new Date(tst * 1000); // Convertendo timestamp UNIX para formato de data
     const query = `
-          INSERT INTO localizacao (device_id, latitude, longitude, velocidade, timestamp, bateria)
-          VALUES ($1, $2, $3, $4, $5, $6)
-      `;
+            INSERT INTO localizacao (device_id, latitude, longitude, velocidade, timestamp, bateria)
+            VALUES ($1, $2, $3, $4, $5, $6)
+        `;
     await pool.query(query, [tid, lat, lon, vel || 0, timestamp, batt || 100]);
 
-    res.status(200).json({ message: 'Dados recebidos com sucesso!' });
+    // Garantir que uma resposta seja enviada corretamente
+    return res.status(200).json({ message: 'Dados recebidos com sucesso!' });
   } catch (error) {
     console.error('Erro ao processar dados: ', error);
-    res.status(500).json({ message: 'Erro no servidor' });
+    // Enviar resposta de erro caso algo dê errado
+    return res.status(500).json({ message: 'Erro no servidor ao processar dados.' });
   }
 });
 
@@ -47,18 +54,18 @@ app.post('/', async (req, res) => {
 app.get('/locations', async (req, res) => {
   try {
     const query = `
-          SELECT device_id, latitude, longitude, velocidade, timestamp, bateria
-          FROM localizacao
-          ORDER BY timestamp DESC
-      `;
+            SELECT device_id, latitude, longitude, velocidade, timestamp, bateria
+            FROM localizacao
+            ORDER BY timestamp DESC
+            LIMIT 50
+        `;
     const result = await pool.query(query);
-    res.status(200).json(result.rows);
+    return res.status(200).json(result.rows);
   } catch (error) {
     console.error('Erro ao buscar localizações: ', error);
-    res.status(500).json({ message: 'Erro ao buscar localizações' });
+    return res.status(500).json({ message: 'Erro ao buscar localizações' });
   }
 });
-
 
 // Iniciar o servidor na porta 3000
 app.listen(3000, '0.0.0.0', () => {
