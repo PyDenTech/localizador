5cb3d3ca88e5a9b7e233d16814e2f4ad48de0153
Danilo <danilo.m.gustavo@gmail.com>
2024-09-30 13:36:13 -0300

    Atualização de localizador

--- server.js
+++ server.js
@@ -24,26 +24,29 @@ const pool = new Pool({
 // Servir arquivos estáticos do diretório "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rota principal para receber dados do OwnTracks (ou Traccar)
 app.post('/', async (req, res) => {
   try {
+    console.log('Recebido:', req.body); // Log do payload recebido
+
     const { _type, lat, lon, vel, tst, tid, batt } = req.body;
 
     // Verificar se o tipo de mensagem é "location" (próprio do OwnTracks)
     if (_type !== 'location') {
+      console.error('Tipo de mensagem inválido!', req.body);
       return res.status(400).json({ message: 'Tipo de mensagem inválido!' });
     }
 
     // Verifica se os dados obrigatórios estão presentes
     if (!lat || !lon || !tst || !tid) {
+      console.error('Dados de localização inválidos!', req.body);
       return res.status(400).json({ message: 'Dados de localização inválidos!' });
     }
 
     const timestamp = new Date(tst * 1000); // Convertendo timestamp UNIX para formato de data
     const query = `
-            INSERT INTO localizacao (device_id, latitude, longitude, velocidade, timestamp, bateria)
-            VALUES ($1, $2, $3, $4, $5, $6)
-        `;
+          INSERT INTO localizacao (device_id, latitude, longitude, velocidade, timestamp, bateria)
+          VALUES ($1, $2, $3, $4, $5, $6)
+      `;
     await pool.query(query, [tid, lat, lon, vel || 0, timestamp, batt || 100]);
 
     return res.status(200).json({ message: 'Dados recebidos com sucesso!' });
@@ -52,6 +55,7 @@ app.post('/', async (req, res) => {
     return res.status(500).json({ message: 'Erro no servidor ao processar dados.' });
   }
 });
+
 
 // Rota para buscar as últimas localizações de todos os dispositivos
 app.get('/locations', async (req, res) => {
